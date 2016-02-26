dce1ba87b1c612234888d347358bce6450c39dce
Rishi <rishiloyola98245@gmail.com>
2016-02-27 01:17:08 +0530

    Update hackerrank_plugin.py

--- hackerrank_plugin.py
+++ hackerrank_plugin.py
@@ -1,4 +1,9 @@
 import sublime, sublime_plugin
+import urllib2, httplib, urllib
+import json as JSON
+import time
+
+basicUrl = 'https://www.hackerrank.com/rest/contests/'
 
 lang = {'py':'python', 'java':'java', 'c':'c', 'cpp':'cpp'}
 
@@ -10,7 +15,62 @@ class HackerRank():
 		self.url = 'https://www.hackerrank.com/rest/contests/master/challenges/%s/compile_tests'%problem
 		self.submissionId = None
 
-	def run():
+	def getCredentials(self):
+   		from os.path import expanduser
+   	  	import os
+	  	home = expanduser("~")+'/'
+
+	  	try:
+	  		f = open(home+'account.txt', 'r')
+	  	except IOError:
+	  		return None, None, None
+	  	text = f.readlines()
+	  	return True, text[0], text[1]
+	
+	def getParams(self, username, password):
+	  authController = urllib2.HTTPBasicAuthHandler()
+	  authController.add_password( realm=None, uri=basicUrl, user=username, passwd=password)
+	  opener = urllib2.build_opener(authController)
+	  urllib2.install_opener(opener)
+	  return
+
+	def run(self):
+		status, username, password = self.getCredentials()
+		if not status:
+   	  		print "[Error] : Enter your username & password in home/account.txt' \n\n"
+   	  		return
+   	  	self.getParams(username.strip('\n'), password.strip('\n'))
+   	  	try:
+   	  		jsonRequest = {"code":self.code,"language":self.lang,"customtestcase":'false'}
+   	  		sock = urllib2.Request(self.url, urllib.urlencode(jsonRequest))
+	 		resp = urllib2.urlopen(sock)
+	  		header = resp.read()
+	  		header = JSON.loads(header)
+	  		codeId = header['model']['id']
+	  		sock = urllib2.Request(self.url + '/' + str(codeId) + '?')
+	  		jsonResponse = 'NULL'
+	  		i = 0
+	  		while i < 4:
+	  			resp = urllib2.urlopen(sock)
+	  			jsonResponse = JSON.loads(resp.read())
+	  			if len(jsonResponse['model']['testcase_message']) is not 0:
+	  				break
+	  			else:
+	  				time.sleep(1)
+	  				if i > 8:
+	  					print "\n\nCONNECTION TIMED OUT\n\n"
+	  					return
+	  				i += 1
+	  		self.printOutput(codeId, jsonResponse)
+	  	except (urllib2.HTTPError) as (e):
+			print "Something went wrong. Check the problem slug"
+		except (urllib2.URLError) as (e):
+			print "Something went wrong buddy, try again"
+	
+	def printOutput(self, codeId, jsonResponse):
+		print "\n"+'SUBMISSION ID:', str(codeId)+"\n"
+		print "[Output] : "
+		print jsonResponse
 
 
 class RunCommand(sublime_plugin.TextCommand):
